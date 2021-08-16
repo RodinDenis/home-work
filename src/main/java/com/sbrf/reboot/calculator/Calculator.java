ac0d8cac7ab2c0d352c6278057d3ce9c79b3edd2
paviol88 <sergunin.pavel@gmail.com>
2021-08-16 13:56:49 +0300

    Домашка урок 2.2

--- src/main/java/com/sbrf/reboot/calculator/Calculator.java
+++ src/main/java/com/sbrf/reboot/calculator/Calculator.java
@@ -1,9 +1,6 @@
 package com.sbrf.reboot.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
-
-    }
     public static double getAddition(double x, double y) {
         return x + y;
     }
