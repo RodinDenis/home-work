a7f97f643b0ce1d3f52ca82898241f5c6b3560fb
paviol88 <sergunin.pavel@gmail.com>
2021-08-16 13:27:03 +0300

    Домашка урок 2

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -3,4 +3,4 @@ package com.sbrf.reboot;
 public class Main {
     public static void main(String[] args) {
     }
-}
+}  
---
+++ src/main/java/com/sbrf/reboot/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.sbrf.reboot.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+    public static double getAddition(double x, double y) {
+        return x + y;
+    }
+    public static double getSubtraction(double x, double y) {
+        return x - y;
+    }
+    public static double getMultiplication(double x, double y) {
+        return x * y;
+    }
+    public static double getDivision(double x, double y) {
+        return x / y;
+    }
+    public static double getSquare(double x) {
+        return x * x;
+    }
+    public static double getReverse(double x) {
+       return 1 / x;
+    }
+    public static double getSqrt(double x) {
+        return Math.sqrt(x);
+    }
+}
