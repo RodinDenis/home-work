51001c3933b162fe701a864c37350f0ff9eb31f8
Kserrg <opto17@mail.ru>
2021-09-01 22:09:21 +0300

    less2 end

---
+++ src/main/java/com/sbrf/reboot/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package com.sbrf.reboot.calculator;
+
+public class Calculator {
+    public static int getAddition(int i, int i1) {
+        return i + i1;
+    }
+
+    public static int getSubtraction(int i, int i1) {
+        return i - i1;
+    }
+
+    public static int getMultiplication(int i, int i1) {
+        return i * i1;
+    }
+
+    public static int getDivision(int i, int i1) {
+        return i / i1;
+    }
+
+    public static int meth5(int i, int i1) {
+        return i / i1;
+    }
+
+    public static int meth6(int i, int i1) {
+        return i / i1;
+    }
+
+    public static int meth7(int i, int i1) {
+        return i / i1;
+    }
+}
