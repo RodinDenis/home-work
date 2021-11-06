6f2338cacb477d67306935ffbc3a7c1c98ddf8f1
Войнова Ольга Евгеньевна <voynova.o.ev@sberbank.ru>
2021-11-06 22:14:46 +0300

    OlgaVoynova Lesson2

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -2,5 +2,12 @@ package com.sbrf.reboot;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Calculator.getAddition(1,2));
+        System.out.println(Calculator.getSubtraction(-5,-6));
+        System.out.println(Calculator.getDivision(10,3));
+        System.out.println(Calculator.getMultiplication(100,100));
+        System.out.println(Calculator.getLogarifm(-1));
+        System.out.println(Calculator.getSquareRoot(150));
+        System.out.println(Calculator.getFactorial(0));
     }
 }
