afdeae7e8925e206439edc235c6f6c5bea667176
Roman Kolganov <Kolganov.RA@yandex.ru>
2021-11-14 21:03:44 +0400

    Kolganov lesson 3

---
+++ src/main/java/com/sbrf/reboot/service/Account.java
@@ -0,0 +1,10 @@
+package com.sbrf.reboot.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Account {
+    private String accountNumber;
+}
