5cee02eb9dea88f8bce575c645f5cd8791558d3b
Emirkhan <emdzhaparov@edu.hse.ru>
2021-12-29 21:43:38 +0300

    Выполнил дз к уроку 2. Добавил 3 своих метода и 4, требуемых по условию. Добавил свои тесты ко всем методам.

--- src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
+++ src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
@@ -1,10 +1,11 @@
 package com.sbrf.reboot.calculator;
 
+import com.sbrf.reboot.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CalculatorTest {
+class   CalculatorTest {
 
     @Test
     void getAddition() {
@@ -25,6 +26,37 @@ class CalculatorTest {
     void getDivision() {
         assertEquals(3, new Calculator().getDivision(9, 3));
     }
+
+    @Test
+    void getAdditionTest1() { assertEquals(100, new Calculator().getAddition(100, 0)); }
+
+    @Test
+    void getSubtractionTest1() {
+        assertEquals(0, new Calculator().getSubtraction(777, 777));
+    }
+
+    @Test
+    void getMultiplicationTest1() {
+        assertEquals(1000, new Calculator().getMultiplication(20, 50));
+    }
+
+    @Test
+    void getDivisionTest1() {
+        assertEquals(7, new Calculator().getDivision(63, 9));
+    }
+
+    @Test
+    void getPowTest0() {
+        assertEquals(64, new Calculator().getPow(2, 6));
+    }
+
+    @Test
+    void getFactorialTest0() {
+        assertEquals(40320, new Calculator().getFactorial(8));
+    }
+
+    @Test
+    void getGCDTest0() { assertEquals(25, new Calculator().getGCD(125, 75)); }
 
     @Test
     void classHasSevenMethods() {
