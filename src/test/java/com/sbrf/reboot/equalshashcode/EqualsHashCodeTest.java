b5a5450bf1a9d7fc7e8c33cbe2b9da4ac0191d63
User <89228067141afmit@gmail.com>
2022-01-23 15:43:34 +0300

    Переопределены методы Equals и HashCode, добавлены тесты для проверки выполения контракта между ними. Все тесты пройдены

--- src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
+++ src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
@@ -5,30 +5,60 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class EqualsHashCodeTest {
 
-     class Car {
+    class Car {
         String model;
         String color;
         Calendar releaseDate;
         int maxSpeed;
 
         @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 41 * hash + Objects.hashCode(this.model);
+            hash = 41 * hash + Objects.hashCode(this.color);
+            hash = 41 * hash + Objects.hashCode(this.releaseDate);
+            hash = 41 * hash + this.maxSpeed;
+            return hash;
+        }
+
+        @Override
         public boolean equals(Object o) {
 
             //Рефлексивность: объект должен равняться самому себе
-            if (o == this)
+            if (this == o)
                 return true;
 
-            return false;
+            // Сравнение null
+            if (o == null)
+                return false;
+
+            Car toCompare = (Car) o;
+
+            // Далее - сравнение значимых полей:
+            if (!this.model.equals(toCompare.model))
+                return false;
+
+
+            if (!this.color.equals(toCompare.color))
+                return false;
+
+
+            if (!this.releaseDate.equals(toCompare.releaseDate))
+                return false;
+
+
+            return true;
         }
 
 
-     }
+    }
 
     @Test
-    public  void assertTrueEquals() {
+    public void assertTrueEquals() {
         Car car1 = new Car();
         car1.model = "Mercedes";
         car1.color = "black";
@@ -63,7 +93,7 @@ public class EqualsHashCodeTest {
     }
 
     @Test
-    public void successEqualsHashCode(){
+    public void successEqualsHashCode() {
         Car car1 = new Car();
         car1.model = "Mercedes";
         car1.color = "black";
@@ -76,12 +106,12 @@ public class EqualsHashCodeTest {
         car2.releaseDate = new GregorianCalendar(2020, 0, 25);
         car2.maxSpeed = 10;
 
-        Assertions.assertEquals(car1.hashCode(),car2.hashCode());
+        Assertions.assertEquals(car1.hashCode(), car2.hashCode());
 
     }
 
     @Test
-    public void failEqualsHashCode(){
+    public void failEqualsHashCode() {
         Car car1 = new Car();
         car1.model = "Mercedes";
         car1.color = "black";
@@ -94,9 +124,50 @@ public class EqualsHashCodeTest {
         car2.releaseDate = new GregorianCalendar(2017, 0, 25);
         car2.maxSpeed = 10;
 
-        Assertions.assertNotEquals(car1.hashCode(),car2.hashCode());
+        Assertions.assertNotEquals(car1.hashCode(), car2.hashCode());
 
     }
 
+    // При равенстве HashCode для двух объектов, Equals точно должен возвращать true
+    @Test
+    public void trueHashCodeThenTrueEquals() {
+        Car car1 = new Car();
+        car1.model = "Mercedes";
+        car1.color = "black";
+        car1.releaseDate = new GregorianCalendar(2020, 0, 25);
+        car1.maxSpeed = 10;
+
+        Car car2 = new Car();
+        car1.model = "Mercedes";
+        car1.color = "black";
+        car1.releaseDate = new GregorianCalendar(2020, 0, 25);
+        car1.maxSpeed = 10;
+
+        boolean hashCompareResult = car1.hashCode() == car2.hashCode();
+
+        Assertions.assertEquals(hashCompareResult, car1.equals(car2));
+
+    }
+
+
+    @Test
+    public void falseHashCodeThenFalseEquals() {
+        Car car1 = new Car();
+        car1.model = "Mercedes";
+        car1.color = "black";
+        car1.releaseDate = new GregorianCalendar(2020, 0, 25);
+        car1.maxSpeed = 10;
+
+        Car car2 = new Car();
+        car2.model = "Audi";
+        car2.color = "white";
+        car2.releaseDate = new GregorianCalendar(2017, 0, 25);
+        car2.maxSpeed = 10;
+
+        boolean hashCompareResult = car1.hashCode() == car2.hashCode();
+
+        Assertions.assertEquals(hashCompareResult, car1.equals(car2));
+
+    }
 
 }
