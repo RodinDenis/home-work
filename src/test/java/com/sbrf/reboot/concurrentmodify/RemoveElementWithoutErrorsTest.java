8c341ba4133112d8036c01aca78b999dfac3ed2a
User <89228067141afmit@gmail.com>
2022-01-27 20:50:00 +0300

    Добавлены реализации удаления объекта из коллекции. Тесты пройдены

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
@@ -20,17 +22,52 @@ public class RemoveElementWithoutErrorsTest {
         }};
 
         assertThrows(ConcurrentModificationException.class, () -> {
-            
+
             for (Integer integer : list) {
                 list.remove(1);
             }
-            
+
+
         });
 
     }
 
     @Test
     public void successRemoveElementWithoutErrors() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+
+        assertDoesNotThrow(() -> {
+
+            list.stream().filter(i -> list.indexOf(i) == 1).collect(Collectors.toList());
+
+        });
+
+    }
+
+
+    @Test
+    public void successRemoveElementWithoutErrors1() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+
+        List<Integer> listToRemove = new ArrayList();
+
+        assertDoesNotThrow(() -> {
+
+            for (Integer integer : list) {
+                if (list.indexOf(integer) == 1) {
+                    listToRemove.add(integer);
+                }
+            }
+            list.removeAll(listToRemove);
+        });
 
     }
 
