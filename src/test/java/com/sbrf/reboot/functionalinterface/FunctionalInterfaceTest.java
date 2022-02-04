f2de0ff6fb0b074ae02f2bac931977bc49a76512
Emirkhan <emdzhaparov@edu.hse.ru>
2022-02-04 18:02:35 +0300

    Выполнил дз за 10-ое занятие. Реализовал класс ListConverter<T>, в котором происходит конвертация. В лямбда-выражении реализовал вариант конвертации каждого объекта в его JSON-представление через JSON-сериализацию: ObjectMapper. Добавил дополнительно свой функциональный интерфейс для конвертации объектов коллекции в XML. Соответствующий метод был добавлен в ListConverter. Соответствующая лямбда была реализована через XML-сериализацию: XmlMapper. ПРИМЕЧАНИЕ: Я знаю, что я мог XML-конвертацию сделать и через исходный функциональный интерфейс, поскольку сигнатуры абстрактных методов совпадают, но поскольку у этих методов разные по логике названия, то я решил создать отдельный функциональный интерфейс и отдельный метод в классе ListConverter. Я уверен, что это и есть дополнительный функционал из доп задания.

--- src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
+++ src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
@@ -1,6 +1,10 @@
 package com.sbrf.reboot.functionalinterface;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.sbrf.reboot.functionalinterface.FunctionalInterfaceTest.SomeObject;
+import java.util.Collection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,25 +30,57 @@ public class FunctionalInterfaceTest {
         String applyAsJson(T t) throws JsonProcessingException;
     }
 
+    /**
+     * Кастомный функ-й интерфейс для конвертирования объектов в XML.
+     */
+    @FunctionalInterface
+    interface ObjectToXMLFunction<T> {
+        String applyAsXML(T t) throws JsonProcessingException;
+    }
+
     static class ListConverter<T> {
-        public List<String> toJsonsList(@NonNull List<T> someObjects, ObjectToJsonFunction<T> objectToJsonFunction) {
+        public List<String> toJsonsList(@NonNull List<T> someObjects, ObjectToJsonFunction<T> objectToJsonFunction)
+            throws JsonProcessingException {
             List<String> result = new ArrayList<>();
             if (someObjects.isEmpty())
                 throw new IllegalArgumentException("The list is empty");
 
             //add code here...
+            for(T elem : someObjects){
+                result.add(objectToJsonFunction.applyAsJson(elem));
+            }
+
+            return result;
+        }
+
+        /**
+         * Добавлен мой кастомный метод для конвертирования объектов в XML.
+         * По сути, он ничем не отличался бы от метода выше, поэтому я создал
+         * дополнительный функциональный интерфейс ObjectToXMLFunction<T>
+         */
+        public List<String> toXMLsList(@NonNull List<T> someObjects, ObjectToXMLFunction<T> objectToXMLFunction)
+            throws JsonProcessingException {
+            List<String> result = new ArrayList<>();
+            if (someObjects.isEmpty())
+                throw new IllegalArgumentException("The list is empty");
+
+            //add code here...
+            for(T elem : someObjects){
+                result.add(objectToXMLFunction.applyAsXML(elem));
+            }
 
             return result;
         }
     }
 
     @Test
-    public void successCallFunctionalInterface() {
+    public void successCallFunctionalInterface() throws JsonProcessingException {
         ListConverter<SomeObject> ListConverter = new ListConverter<>();
 
         ObjectToJsonFunction<SomeObject> objectToJsonFunction = someObject -> {
             //add code here...
-            return null;
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(someObject);
         };
 
         List<String> strings = ListConverter.toJsonsList(
@@ -57,6 +93,32 @@ public class FunctionalInterfaceTest {
 
         Assertions.assertTrue(strings.contains("{\"objectName\":\"Object-1\"}"));
         Assertions.assertTrue(strings.contains("{\"objectName\":\"Object-2\"}"));
+    }
+
+
+    /**
+     * Проверка конвертирования в XML.
+     */
+    @Test
+    public void successCallFunctionalInterfaceXML() throws JsonProcessingException {
+        ListConverter<SomeObject> ListConverter = new ListConverter<>();
+
+        ObjectToXMLFunction<SomeObject> objectToXmlFunction = someObject -> {
+            // my code...
+            XmlMapper xmlMapper = new XmlMapper();
+            return xmlMapper.writeValueAsString(someObject);
+        };
+
+        List<String> strings = ListConverter.toXMLsList(
+            Arrays.asList(
+                new SomeObject("Object-1"),
+                new SomeObject("Object-2")
+            ),
+            objectToXmlFunction
+        );
+
+        Assertions.assertTrue(strings.contains("<SomeObject><objectName>Object-1</objectName></SomeObject>"));
+        Assertions.assertTrue(strings.contains("<SomeObject><objectName>Object-2</objectName></SomeObject>"));
     }
 
 }
